0f5ced83408b458a7b18b8e40d454abcddc08454
Ada <adrianna.kucinska@onet.eu>
2017-09-23 21:45:15 +0200

    Add solution for task 2

--- people/person.rb
+++ people/person.rb
@@ -1,8 +1,9 @@
 class Person
-  def initialize(id, name="Unknown", age)
+  def initialize(name="Unknown", age, parent_permission)
     @id = Random.rand
     @name = name
     @age = age
+    @parent_permission = parent_permission
   end
 
   def name
@@ -20,13 +21,26 @@ class Person
   def age=(value)
     @age = value
   end
+
+  def parent_permission
+    @parent_permission
+  end
+
+  def parent_permission=(value)
+    @parent_permission = value
+  end
   
   def id
     @id
   end
 
+  def can_use_services?
+    is_of_age? || @parent_permission    
+  end
+
+  private
+
   def is_of_age?
     age >= 18
   end
 end
-
