37dd0fc1322a8271e197c3b4c3e57e969853e5e6
Ada <adrianna.kucinska@onet.eu>
2017-09-28 22:54:39 +0200

    Edit correct_name method to 10 chars

--- people/corrector.rb
+++ people/corrector.rb
@@ -1,7 +1,7 @@
 class Corrector
   def correct_name(word, chars = 10)
     if word.length > chars
-      word.capitalize.slice(0..9) + "..." 
+      word.capitalize.slice(0...7) + "..." 
     else 
       word.capitalize
     end    
