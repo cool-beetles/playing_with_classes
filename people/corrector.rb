0ada5836322f6aeed5f2604e806260e511f50a5c
Ada <adrianna.kucinska@onet.eu>
2017-09-28 18:26:31 +0200

    Use 'if' in correct_name method

--- people/corrector.rb
+++ people/corrector.rb
@@ -1,6 +1,9 @@
 class Corrector
-  def correct_name(word)
-    word.capitalize!
-    word.slice(0..9) + "..."      
+  def correct_name(word, chars = 10)
+    if word.length > chars
+      word.capitalize.slice(0..9) + "..." 
+    else 
+      word.capitalize
+    end    
   end
 end
