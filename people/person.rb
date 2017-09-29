4bce55bc11d09875bdbcf341395569c51ee5a3c8
Ada <adrianna.kucinska@onet.eu>
2017-09-29 15:06:42 +0200

    #8 Add solution for task 8

--- people/person.rb
+++ people/person.rb
@@ -1,7 +1,7 @@
 require "./people/corrector.rb"
 class Person
 
-  attr_reader :id :label
+  attr_reader :id :label :rentals
   attr_accessor :name :age :parent_permission
 
   def initialize(name = "Unknown", age, parent_permission = true)
@@ -9,11 +9,17 @@ class Person
     @name = name
     @age = age
     @parent_permission = parent_permission
+    @rentals = []
   end
 
   def label=(value)
     @label = value
     value.students.push(self) unless value.students.include?(self)
+  end
+
+  def add_visit(rental)
+    @rentals << rental
+    rental.person = self
   end
 
   def can_use_services?
